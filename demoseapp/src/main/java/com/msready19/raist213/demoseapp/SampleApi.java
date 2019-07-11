6d64125d94b973233a4e0b4068518405b598c093
Felip Miguel Puig <fmiguel@microsoft.com>
2019-07-11 12:56:01 +0200

    sd

--- demoseapp/src/main/java/com/msready19/raist213/demoseapp/SampleApi.java
+++ demoseapp/src/main/java/com/msready19/raist213/demoseapp/SampleApi.java
@@ -3,6 +3,7 @@ package com.msready19.raist213.demoseapp;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,8 @@ public class SampleApi {
         return source;
     }
 
-    @RequestMapping("/reverse")
-    String reverse(String source) {
+    @RequestMapping(path = "/reverse", method=RequestMethod.GET)
+    String reverse(@RequestParam String source) {
         return source;
     }
 }
